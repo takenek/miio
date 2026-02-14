3fc7750391eeadd9d313dcbc84ae6895dc089669
takenek <github.com@wset.edu.pl>
2026-02-14 14:01:45 +0100

    Handle EINTR/EALREADY in connect retry path

---
+++ test/connect-to-device-recovery.test.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const test = require('node:test');
+const assert = require('node:assert/strict');
+
+const network = require('../lib/network');
+const connectToDevice = require('../lib/connectToDevice');
+
+test('connectToDevice retries transient EINTR failures and triggers recovery', async () => {
+	const originalRef = network.ref;
+	const originalFindDeviceViaAddress = network.findDeviceViaAddress;
+	const originalResetSocket = network.resetSocket;
+	const originalRequestRecoveryDiscovery = network.requestRecoveryDiscovery;
+	const originalSetTimeout = global.setTimeout;
+
+	let refCount = 0;
+	let releaseCount = 0;
+	let findAttempts = 0;
+	const recoveryReasons = [];
+
+	global.setTimeout = (handler, timeout, ...args) => {
+		const timer = originalSetTimeout(handler, timeout, ...args);
+		timer.unref = () => timer;
+		return timer;
+	};
+
+	try {
+		network.ref = () => {
+			refCount++;
+			return {
+				release() {
+					releaseCount++;
+				}
+			};
+		};
+
+		network.resetSocket = reason => {
+			recoveryReasons.push(['resetSocket', reason]);
+		};
+
+		network.requestRecoveryDiscovery = reason => {
+			recoveryReasons.push(['requestRecoveryDiscovery', reason]);
+		};
+
+		network.findDeviceViaAddress = async () => {
+			findAttempts++;
+			if (findAttempts === 1) {
+				const err = new Error('interrupted system call');
+				err.code = 'EINTR';
+				throw err;
+			}
+
+			throw new Error('non transient failure');
+		};
+
+		await assert.rejects(
+			connectToDevice({
+				address: '127.0.0.1',
+				port: 54321,
+				connectionRetries: 1
+			}),
+			error => {
+				assert.match(error.message, /non transient failure/);
+				return true;
+			}
+		);
+
+		assert.equal(findAttempts, 2);
+		assert.deepEqual(
+			recoveryReasons.map(entry => entry[0]),
+			['resetSocket', 'requestRecoveryDiscovery']
+		);
+		assert.match(recoveryReasons[0][1], /connect retry after transient error: EINTR/);
+		assert.ok(refCount >= 1);
+		assert.ok(releaseCount >= 1);
+	} finally {
+		network.ref = originalRef;
+		network.findDeviceViaAddress = originalFindDeviceViaAddress;
+		network.resetSocket = originalResetSocket;
+		network.requestRecoveryDiscovery = originalRequestRecoveryDiscovery;
+		global.setTimeout = originalSetTimeout;
+	}
+});
